15770e09a9668bd1575fd1af1af57d690fea9561
Kayum Ali <sisirfahimstudent173@gmail.com>
2024-03-16 16:23:48 +0600

    tailwind setup and header setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header></Header>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-teal-200 p-5 bg-cyan-500'>Knowladge Cafe</h1>
+
+
+        </div>
+    );
+};
+
+export default Header;
